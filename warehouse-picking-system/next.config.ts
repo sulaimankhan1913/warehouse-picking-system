cf5e858eb2e76905b1bbc266a2d03a92aa194b4f
sulaimankhan1913 <sulaimanelkhan@gmail.com>
2026-07-17 15:21:25 +0800

    Update next.config.ts

--- warehouse-picking-system/next.config.ts
+++ warehouse-picking-system/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdfjs-dist"],
+serverExternalPackages: ["@napi-rs/canvas", "pdfjs-dist"],
 };
 
 export default nextConfig;
